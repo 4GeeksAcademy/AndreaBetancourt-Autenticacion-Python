82264a65db447623c6a2c4ee7783ea3eddfdb1cd
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-09-30 18:33:12 +0000

    Entrega final

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,28 +9,28 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
 		<div className="container-fluid">
-			<a className="navbar-brand" href="#">JWT Auth Assessment</a>
+			<Link to='/' className="navbar-brand">JWT Auth Assessment</Link>
 			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 			</button>
 			<div className="collapse navbar-collapse" id="navbarColor03">
 			
-				<Link to="/">
-				<a className="nav-link active" href="#">Home</a>
+				<Link to="/" className="nav-link active">
+					Home
 				</Link>
 				
 			<form className="d-flex">
-				<Link to="/signup">
-				<a className="nav-link active" href="#">Sign up</a>
+				<Link to="/signup" className="nav-link active">
+					Sign up
 				</Link>	
 				{/* Si el toquen no existe va a log in para ingresar */}
 				{!store.token ? (
-				<Link to="/login">
-					<a className="nav-link active" href="#">Log in</a>
+				<Link to="/login" className="nav-link active">
+					Log in
 				</Link>
 				):(
-				<Link to="/login">
-					<a className="nav-link active" href="#" onClick={() => actions.logout()}>Log out</a>
+				<Link to="/login" className="nav-link active" onClick={() => actions.logout()}>
+					Log out
 				</Link>			
 				)}
 			</form>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,6 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			token: null,
+			users: [],
 			message: null,
 			demo: [
 				{
@@ -17,12 +18,64 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+			getUsers: async () => {
 
-			login : async (email, password) => {
+				try {
+					const resp = await fetch(process.env.BACKEND_URL + "/api/users")
+
+					if (resp.status !== 200) alert('Error, try again');
+
+					const data = await resp.json()
+					sessionStorage.setItem('users', JSON.stringify(data))
+					setStore({ 'users': data })
+					
+				} catch (error) {
+					console.log('Error:', Error)
+					alert("An error occurred. Please try again.")
+				}
+
+			},
+			signup: async (email, password) => {
+				const requestOptions = {
+					method: "POST",
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				};
+			
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/api/register", requestOptions);
+			
+					if (response.status === 200) {
+						const data = await response.json();
+						console.log("Response from backend:", data); // Verifica la respuesta del backend en la consola
+						
+						// Verificar si el mensaje del backend indica que el usuario se registró con éxito y si contiene "new user"
+						if (data.message === "User registered successfully" && data.hasOwnProperty("new user")) {
+							// Almacenar los datos del usuario en sessionStorage
+							sessionStorage.setItem('users', JSON.stringify(data['new user']));
+							setStore({ users: data['new user'] });
+							alert('Registration successful. You can now log in.');
+						} else {
+							// Manejar otros casos de respuesta
+							alert('Registration failed. Please try again.');
+						}
+					} else {
+						// Error del servidor
+						alert('Registration failed. Please try again.');
+					}
+				} catch (error) {
+					// Error de red u otro error
+					console.error("Error registering user:", error);
+					alert("An error occurred. Please try again.");
+				}
+			},			
+			login: async (email, password) => {
 				// 1. Definir las opciones para la solicitud POST
 				const requestsOpts = {
 					method: "POST",
@@ -36,27 +89,27 @@ const getState = ({ getStore, getActions, setStore }) => {
 						"password": password
 					})
 				};
-		
+
 				try {
 					// 2. Realizar la solicitud POST al endpoint de token
 					const resp = await fetch(process.env.BACKEND_URL + "/api/token", requestsOpts);
-					
+
 					// 2.1 Se verifica si la respuesta del servidor tiene un estado diferente de 200 y si es asi generar alerta y return False
 					if (resp.status !== 200) {
 						alert('There has been some error')
 						return false;
 					}
-					
+
 					//2.2 Si la respuesta del servidor tiene un estado 200, se obtiene el cuerpo de la respuesta utilizando resp.json(). 
 					const data = await resp.json();
 					console.log('This came form de backen', data)
-					
+
 					// 2.3. Se almacena el token en el sessionStorage del navegador y se actualiza el estado de la aplicación utilizando la función setStore.
 					sessionStorage.setItem("token", data.token);
-					setStore({token: data.token})
+					setStore({ token: data.token })
 					return true; //Devolver true si el inicio de sesión es exitoso
-					
-				// 3. Si en cualquier punto se produce un error, se captura el error y se muestra en la consola.
+
+					// 3. Si en cualquier punto se produce un error, se captura el error y se muestra en la consola.
 				} catch (error) {
 					console.error('There has been some error!', error);
 					alert("Registration failed. Please try again later.")
@@ -68,9 +121,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//1. Obtener el token del sessionStorage
 				const token = sessionStorage.getItem('token')
 				//1.1 Verificar si el token existe y no está vacío ni es undefined
-				if (token && token != '' && typeof token != undefined){
+				if (token && token != '' && typeof token != undefined) {
 					// 1.2. Actualizar el estado global de la aplicación con el token
-					setStore({token: token})
+					setStore({ token: token })
 				}
 			},
 
@@ -83,7 +136,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 
@@ -96,7 +149,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						console.log("Received an unexpected status code:", resp.status);
 						return null;
 					}
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error);
 					throw error;
 				}
